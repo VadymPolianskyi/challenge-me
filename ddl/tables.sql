841d3c53ba48522839e27ea63d3cc633794fba24
Vadym Polianskyi <icvadym.polianskyi@americanwell.com>
2021-11-18 17:13:58 +0000

    Add table's DDLs

---
+++ ddl/tables.sql
@@ -0,0 +1,39 @@
+CREATE TABLE "user" (
+  username varchar NOT NULL,
+  "name" varchar,
+  age integer,
+  token varchar,
+  email varchar,
+  PRIMARY KEY(username)
+);
+
+CREATE TABLE challenge(
+  id varchar NOT NULL,
+  "name" varchar,
+  description varchar,
+  "from" timestamp,
+  "until" timestamp,
+  created timestamp,
+  frequency varchar("HOUR", "DAY", "WEEK", "MONTH"),
+  price integer,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE payment(
+  id varchar NOT NULL,
+  "user" varchar NOT NULL,
+  payment_system varchar,
+  card_number varchar,
+  expiration_date date,
+  placeholder varchar,
+  PRIMARY KEY(id),
+  CONSTRAINT "Forein key" UNIQUE("user")
+);
+
+CREATE TABLE participation(
+  challenge_id varchar NOT NULL,
+  "user" varchar NOT NULL,
+  "time" varchar NOT NULL,
+  status varchar,
+  PRIMARY KEY(challenge_id, "time", "user")
+);

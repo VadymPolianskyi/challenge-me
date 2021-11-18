10ba74bc528000691b18e6ca62d2eccb2808c44a
Vadym Polianskyi <icvadym.polianskyi@americanwell.com>
2021-11-18 19:12:28 +0000

    Remove userDao mock

--- project/db/user.py
+++ project/db/user.py
@@ -46,27 +46,3 @@ class UserDao(UserDaoInterface):
                 conn.commit()
                 print(f"Successfully created user with username '{user.username}'")
         return user
-
-
-###################################
-#             MOCK                #
-###################################
-
-class MockDB(UserDaoInterface):
-    fake_users_db = {
-        "johndoe": {
-            "username": "johndoe",
-            "name": "John Doe",
-            "email": "johndoe@example.com",
-            "age": 20,
-            "token": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
-        }
-    }
-
-    def get_user(self, username: str):
-        if username in self.fake_users_db:
-            user_dict = self.fake_users_db[username]
-            return User(**user_dict)
-
-    def save_user(self, user: User):
-        return user

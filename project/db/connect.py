bcaba38afce0eb6b7aa14283742f0965c4949357
Vadym Polianskyi <icvadym.polianskyi@americanwell.com>
2021-11-19 12:36:26 +0000

    Correct connector logs and apply reusing approach

--- project/db/connect.py
+++ project/db/connect.py
@@ -3,14 +3,11 @@ import psycopg2
 from project.config import reader as config_reader
 
 
-def open_connection_cursor():
-    """ Creates connection to the PostgreSQL database server """
-    print('Connecting to the PostgreSQL database...')
-    conn = psycopg2.connect(**config_reader.db)
-    return conn.cursor()
-
-
 def open_connection():
-    """ Creates connection to the PostgreSQL database server """
     print('Connecting to the PostgreSQL database...')
     return psycopg2.connect(**config_reader.db)
+
+
+def open_connection_cursor():
+    print('Cursor from Connection to the PostgreSQL database...')
+    return open_connection.cursor()
